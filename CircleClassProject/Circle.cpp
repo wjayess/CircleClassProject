8db461b3b8b6ff13621a78404f4072b28b2e9fea
William Smith <wjacobsmith20@outlook.com>
2022-03-30 01:15:32 -0500

    implementation = pretty pls

--- CircleClassProject/Circle.cpp
+++ CircleClassProject/Circle.cpp
@@ -2,25 +2,9 @@
 
 #include "Circle.h"
 
-Circle::Circle(double inRadius)
-{
-	radius = inRadius;
-}
+Circle::Circle(double inRadius) { radius = inRadius; }
 
-double Circle::getRadius() const
-{
-	return radius;
-}
-
-double Circle::getArea() const
-{
-	return pi * radius * radius;
-}
-double Circle::getDiameter() const
-{
-	return radius * 2;
-}
-double Circle::getCircumference() const
-{
-	return 2 * pi * radius;
-}
+double Circle::getRadius() const { return radius; }
+double Circle::getArea() const { return pi * radius * radius; }
+double Circle::getDiameter() const { return radius * 2; }
+double Circle::getCircumference() const { return 2 * pi * radius; }
